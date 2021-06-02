74b47c2c1614f4430550a31452304d6c568be8df
Abri Grobler <grobleraf@gmail.com>
2021-06-02 17:17:31 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
